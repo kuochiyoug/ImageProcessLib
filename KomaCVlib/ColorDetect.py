f0384b20d721b8bc4d28fcf9c78df1a9faa97c89
YANG PIN CHU(Koma) <kcy.komayang@gmail.com>
2017-10-31 01:42:56 +0900

    Add Blue Detect

--- KomaCVlib/ColorDetect.py
+++ KomaCVlib/ColorDetect.py
@@ -14,5 +14,39 @@ class ColorDetect:
             print("Error: No color " + str(color) + " in color label")
             return False
 
-        img_hsv = cv2.cvtColor(img_rgb,CV_BGR2HSV)
-        for
+        img_hsv = cv2.cvtColor(img_rgb,cv2.CV_BGR2HSV)
+        img_medianBlur(img_hsv,7)
+        lower_range = np.array([])
+        upper_range = np.array([])
+
+    def DetectWithCamera(self):
+        cap = cv2.VideoCapture(0)
+        while(1):
+
+            # Take each frame
+            _, frame = cap.read()
+
+            # Convert BGR to HSV
+            hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+            # define range of blue color in HSV
+            lower_blue = np.array([110,50,50])
+            upper_blue = np.array([130,255,255])
+
+            # Threshold the HSV image to get only blue colors
+            mask = cv2.inRange(hsv, lower_blue, upper_blue)
+
+            # Bitwise-AND mask and original image
+            res = cv2.bitwise_and(frame,frame, mask= mask)
+
+            cv2.imshow('frame',frame)
+            cv2.imshow('mask',mask)
+            cv2.imshow('res',res)
+            k = cv2.waitKey(5) & 0xFF
+            if k == 27:
+                break
+
+        cv2.destroyAllWindows()
+
+colclass = ColorDetect()
+colclass.DetectWithCamera()
